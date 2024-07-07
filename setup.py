520f6af9a019a68c26708f554dce580c14488486
Nareen Waseem <113854962+nareenwaseem@users.noreply.github.com>
2024-07-07 14:32:28 +0500

    Packages Installed

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
-conda 
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenrator',
+    version='0.0.1',
+    author='Nareen Waseem',
+    author_email='nareenwaseem15@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
